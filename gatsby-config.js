10c3baead4313fa75c631bb0ba32b6f8b902a714
Ugo Bechameil <ugo.bechameil@realimpactanalytics.com>
2018-05-01 18:43:58 +0200

    black black black

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Din Ou',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,36 +4,30 @@ import Helmet from 'react-helmet'
 
 import Img from "gatsby-image";
 import './index.css'
+import { isAbsolute } from 'path';
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'under construction' },
+        { name: 'keywords', content: 'personal services' },
       ]}
     />
+    <div>
     <Img
       title="Under construction"
       alt="Din Ou's almost there"
       sizes={data.headerImage.sizes}
       style={{
-        position: "absolute",
-        left: 0,
-        top: 0,
-        width: "100%",
+        left: "11%",
+        width: "80%",
         height: "100%"
       }}
     />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    </div>
+    <div>
       {children()}
     </div>
   </div>
